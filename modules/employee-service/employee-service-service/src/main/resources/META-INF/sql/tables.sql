db6bc671f58282ad87989f87d4b7fb6f39bdf40d
Khosla <2505478@cognizant.com>
2026-07-07 15:03:26 +0530

    Added Employee Service Builder module

---
+++ modules/employee-service/employee-service-service/src/main/resources/META-INF/sql/tables.sql
@@ -0,0 +1,12 @@
+create table HRMS_Employee (
+	employeeId LONG not null primary key,
+	employeeCode VARCHAR(75) null,
+	firstName VARCHAR(75) null,
+	lastName VARCHAR(75) null,
+	email VARCHAR(75) null,
+	phoneNumber VARCHAR(75) null,
+	department VARCHAR(75) null,
+	designation VARCHAR(75) null,
+	joiningDate DATE null,
+	status VARCHAR(75) null
+);
---
+++ modules/employee-service/employee-service-test/src/testIntegration/java/com/hrms/employee/service/persistence/test/EmployeePersistenceTest.java
@@ -0,0 +1,434 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.hrms.employee.service.persistence.test;
+
+import com.hrms.employee.exception.NoSuchEmployeeException;
+import com.hrms.employee.model.Employee;
+import com.hrms.employee.service.EmployeeLocalServiceUtil;
+import com.hrms.employee.service.persistence.EmployeePersistence;
+import com.hrms.employee.service.persistence.EmployeeUtil;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.transaction.Propagation;
+import com.liferay.portal.kernel.util.IntegerWrapper;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
+import com.liferay.portal.kernel.util.Time;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PersistenceTestRule;
+import com.liferay.portal.test.rule.TransactionalTestRule;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @generated
+ */
+@RunWith(Arquillian.class)
+public class EmployeePersistenceTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), PersistenceTestRule.INSTANCE,
+			new TransactionalTestRule(
+				Propagation.REQUIRED, "com.hrms.employee.service"));
+
+	@Before
+	public void setUp() {
+		_persistence = EmployeeUtil.getPersistence();
+
+		Class<?> clazz = _persistence.getClass();
+
+		_dynamicQueryClassLoader = clazz.getClassLoader();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Iterator<Employee> iterator = _employees.iterator();
+
+		while (iterator.hasNext()) {
+			_persistence.remove(iterator.next());
+
+			iterator.remove();
+		}
+	}
+
+	@Test
+	public void testCreate() throws Exception {
+		long pk = RandomTestUtil.nextLong();
+
+		Employee employee = _persistence.create(pk);
+
+		Assert.assertNotNull(employee);
+
+		Assert.assertEquals(employee.getPrimaryKey(), pk);
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		Employee newEmployee = addEmployee();
+
+		_persistence.remove(newEmployee);
+
+		Employee existingEmployee = _persistence.fetchByPrimaryKey(
+			newEmployee.getPrimaryKey());
+
+		Assert.assertNull(existingEmployee);
+	}
+
+	@Test
+	public void testUpdateNew() throws Exception {
+		addEmployee();
+	}
+
+	@Test
+	public void testUpdateExisting() throws Exception {
+		long pk = RandomTestUtil.nextLong();
+
+		Employee newEmployee = _persistence.create(pk);
+
+		newEmployee.setEmployeeCode(RandomTestUtil.randomString());
+
+		newEmployee.setFirstName(RandomTestUtil.randomString());
+
+		newEmployee.setLastName(RandomTestUtil.randomString());
+
+		newEmployee.setEmail(RandomTestUtil.randomString());
+
+		newEmployee.setPhoneNumber(RandomTestUtil.randomString());
+
+		newEmployee.setDepartment(RandomTestUtil.randomString());
+
+		newEmployee.setDesignation(RandomTestUtil.randomString());
+
+		newEmployee.setJoiningDate(RandomTestUtil.nextDate());
+
+		newEmployee.setStatus(RandomTestUtil.randomString());
+
+		_employees.add(_persistence.update(newEmployee));
+
+		Employee existingEmployee = _persistence.findByPrimaryKey(
+			newEmployee.getPrimaryKey());
+
+		Assert.assertEquals(
+			existingEmployee.getEmployeeId(), newEmployee.getEmployeeId());
+		Assert.assertEquals(
+			existingEmployee.getEmployeeCode(), newEmployee.getEmployeeCode());
+		Assert.assertEquals(
+			existingEmployee.getFirstName(), newEmployee.getFirstName());
+		Assert.assertEquals(
+			existingEmployee.getLastName(), newEmployee.getLastName());
+		Assert.assertEquals(
+			existingEmployee.getEmail(), newEmployee.getEmail());
+		Assert.assertEquals(
+			existingEmployee.getPhoneNumber(), newEmployee.getPhoneNumber());
+		Assert.assertEquals(
+			existingEmployee.getDepartment(), newEmployee.getDepartment());
+		Assert.assertEquals(
+			existingEmployee.getDesignation(), newEmployee.getDesignation());
+		Assert.assertEquals(
+			Time.getShortTimestamp(existingEmployee.getJoiningDate()),
+			Time.getShortTimestamp(newEmployee.getJoiningDate()));
+		Assert.assertEquals(
+			existingEmployee.getStatus(), newEmployee.getStatus());
+	}
+
+	@Test
+	public void testFindByPrimaryKeyExisting() throws Exception {
+		Employee newEmployee = addEmployee();
+
+		Employee existingEmployee = _persistence.findByPrimaryKey(
+			newEmployee.getPrimaryKey());
+
+		Assert.assertEquals(existingEmployee, newEmployee);
+	}
+
+	@Test(expected = NoSuchEmployeeException.class)
+	public void testFindByPrimaryKeyMissing() throws Exception {
+		long pk = RandomTestUtil.nextLong();
+
+		_persistence.findByPrimaryKey(pk);
+	}
+
+	@Test
+	public void testFindAll() throws Exception {
+		_persistence.findAll(
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS, getOrderByComparator());
+	}
+
+	protected OrderByComparator<Employee> getOrderByComparator() {
+		return OrderByComparatorFactoryUtil.create(
+			"HRMS_Employee", "employeeId", true, "employeeCode", true,
+			"firstName", true, "lastName", true, "email", true, "phoneNumber",
+			true, "department", true, "designation", true, "joiningDate", true,
+			"status", true);
+	}
+
+	@Test
+	public void testFetchByPrimaryKeyExisting() throws Exception {
+		Employee newEmployee = addEmployee();
+
+		Employee existingEmployee = _persistence.fetchByPrimaryKey(
+			newEmployee.getPrimaryKey());
+
+		Assert.assertEquals(existingEmployee, newEmployee);
+	}
+
+	@Test
+	public void testFetchByPrimaryKeyMissing() throws Exception {
+		long pk = RandomTestUtil.nextLong();
+
+		Employee missingEmployee = _persistence.fetchByPrimaryKey(pk);
+
+		Assert.assertNull(missingEmployee);
+	}
+
+	@Test
+	public void testFetchByPrimaryKeysWithMultiplePrimaryKeysWhereAllPrimaryKeysExist()
+		throws Exception {
+
+		Employee newEmployee1 = addEmployee();
+		Employee newEmployee2 = addEmployee();
+
+		Set<Serializable> primaryKeys = new HashSet<Serializable>();
+
+		primaryKeys.add(newEmployee1.getPrimaryKey());
+		primaryKeys.add(newEmployee2.getPrimaryKey());
+
+		Map<Serializable, Employee> employees = _persistence.fetchByPrimaryKeys(
+			primaryKeys);
+
+		Assert.assertEquals(2, employees.size());
+		Assert.assertEquals(
+			newEmployee1, employees.get(newEmployee1.getPrimaryKey()));
+		Assert.assertEquals(
+			newEmployee2, employees.get(newEmployee2.getPrimaryKey()));
+	}
+
+	@Test
+	public void testFetchByPrimaryKeysWithMultiplePrimaryKeysWhereNoPrimaryKeysExist()
+		throws Exception {
+
+		long pk1 = RandomTestUtil.nextLong();
+
+		long pk2 = RandomTestUtil.nextLong();
+
+		Set<Serializable> primaryKeys = new HashSet<Serializable>();
+
+		primaryKeys.add(pk1);
+		primaryKeys.add(pk2);
+
+		Map<Serializable, Employee> employees = _persistence.fetchByPrimaryKeys(
+			primaryKeys);
+
+		Assert.assertTrue(employees.isEmpty());
+	}
+
+	@Test
+	public void testFetchByPrimaryKeysWithMultiplePrimaryKeysWhereSomePrimaryKeysExist()
+		throws Exception {
+
+		Employee newEmployee = addEmployee();
+
+		long pk = RandomTestUtil.nextLong();
+
+		Set<Serializable> primaryKeys = new HashSet<Serializable>();
+
+		primaryKeys.add(newEmployee.getPrimaryKey());
+		primaryKeys.add(pk);
+
+		Map<Serializable, Employee> employees = _persistence.fetchByPrimaryKeys(
+			primaryKeys);
+
+		Assert.assertEquals(1, employees.size());
+		Assert.assertEquals(
+			newEmployee, employees.get(newEmployee.getPrimaryKey()));
+	}
+
+	@Test
+	public void testFetchByPrimaryKeysWithNoPrimaryKeys() throws Exception {
+		Set<Serializable> primaryKeys = new HashSet<Serializable>();
+
+		Map<Serializable, Employee> employees = _persistence.fetchByPrimaryKeys(
+			primaryKeys);
+
+		Assert.assertTrue(employees.isEmpty());
+	}
+
+	@Test
+	public void testFetchByPrimaryKeysWithOnePrimaryKey() throws Exception {
+		Employee newEmployee = addEmployee();
+
+		Set<Serializable> primaryKeys = new HashSet<Serializable>();
+
+		primaryKeys.add(newEmployee.getPrimaryKey());
+
+		Map<Serializable, Employee> employees = _persistence.fetchByPrimaryKeys(
+			primaryKeys);
+
+		Assert.assertEquals(1, employees.size());
+		Assert.assertEquals(
+			newEmployee, employees.get(newEmployee.getPrimaryKey()));
+	}
+
+	@Test
+	public void testActionableDynamicQuery() throws Exception {
+		final IntegerWrapper count = new IntegerWrapper();
+
+		ActionableDynamicQuery actionableDynamicQuery =
+			EmployeeLocalServiceUtil.getActionableDynamicQuery();
+
+		actionableDynamicQuery.setPerformActionMethod(
+			new ActionableDynamicQuery.PerformActionMethod<Employee>() {
+
+				@Override
+				public void performAction(Employee employee) {
+					Assert.assertNotNull(employee);
+
+					count.increment();
+				}
+
+			});
+
+		actionableDynamicQuery.performActions();
+
+		Assert.assertEquals(count.getValue(), _persistence.countAll());
+	}
+
+	@Test
+	public void testDynamicQueryByPrimaryKeyExisting() throws Exception {
+		Employee newEmployee = addEmployee();
+
+		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(
+			Employee.class, _dynamicQueryClassLoader);
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.eq(
+				"employeeId", newEmployee.getEmployeeId()));
+
+		List<Employee> result = _persistence.findWithDynamicQuery(dynamicQuery);
+
+		Assert.assertEquals(1, result.size());
+
+		Employee existingEmployee = result.get(0);
+
+		Assert.assertEquals(existingEmployee, newEmployee);
+	}
+
+	@Test
+	public void testDynamicQueryByPrimaryKeyMissing() throws Exception {
+		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(
+			Employee.class, _dynamicQueryClassLoader);
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.eq(
+				"employeeId", RandomTestUtil.nextLong()));
+
+		List<Employee> result = _persistence.findWithDynamicQuery(dynamicQuery);
+
+		Assert.assertEquals(0, result.size());
+	}
+
+	@Test
+	public void testDynamicQueryByProjectionExisting() throws Exception {
+		Employee newEmployee = addEmployee();
+
+		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(
+			Employee.class, _dynamicQueryClassLoader);
+
+		dynamicQuery.setProjection(
+			ProjectionFactoryUtil.property("employeeId"));
+
+		Object newEmployeeId = newEmployee.getEmployeeId();
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.in(
+				"employeeId", new Object[] {newEmployeeId}));
+
+		List<Object> result = _persistence.findWithDynamicQuery(dynamicQuery);
+
+		Assert.assertEquals(1, result.size());
+
+		Object existingEmployeeId = result.get(0);
+
+		Assert.assertEquals(existingEmployeeId, newEmployeeId);
+	}
+
+	@Test
+	public void testDynamicQueryByProjectionMissing() throws Exception {
+		DynamicQuery dynamicQuery = DynamicQueryFactoryUtil.forClass(
+			Employee.class, _dynamicQueryClassLoader);
+
+		dynamicQuery.setProjection(
+			ProjectionFactoryUtil.property("employeeId"));
+
+		dynamicQuery.add(
+			RestrictionsFactoryUtil.in(
+				"employeeId", new Object[] {RandomTestUtil.nextLong()}));
+
+		List<Object> result = _persistence.findWithDynamicQuery(dynamicQuery);
+
+		Assert.assertEquals(0, result.size());
+	}
+
+	protected Employee addEmployee() throws Exception {
+		long pk = RandomTestUtil.nextLong();
+
+		Employee employee = _persistence.create(pk);
+
+		employee.setEmployeeCode(RandomTestUtil.randomString());
+
+		employee.setFirstName(RandomTestUtil.randomString());
+
+		employee.setLastName(RandomTestUtil.randomString());
+
+		employee.setEmail(RandomTestUtil.randomString());
+
+		employee.setPhoneNumber(RandomTestUtil.randomString());
+
+		employee.setDepartment(RandomTestUtil.randomString());
+
+		employee.setDesignation(RandomTestUtil.randomString());
+
+		employee.setJoiningDate(RandomTestUtil.nextDate());
+
+		employee.setStatus(RandomTestUtil.randomString());
+
+		_employees.add(_persistence.update(employee));
+
+		return employee;
+	}
+
+	private List<Employee> _employees = new ArrayList<Employee>();
+	private EmployeePersistence _persistence;
+	private ClassLoader _dynamicQueryClassLoader;
+
+}
+// LIFERAY-SERVICE-BUILDER-HASH:-418065004
